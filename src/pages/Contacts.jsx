fd116ea7c978b11a3e846a6fadda99f9409aee25
jatinSc <jatin.singhchauhan.4@gmail.com>
2022-12-28 14:52:31 +0530

    contacts page created

---
+++ src/pages/Contacts.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Navbar from "../components/navbar";
+
+const Contact = () => {
+    return(
+        <>
+        
+            <Navbar/>
+           <div className="container mt-3"></div>
+        </>
+    )
+}
+
+export default Contact;
--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -5,6 +5,7 @@ const Login = () => {
         <React.Fragment>
             <p1>This is LogIn Page</p1>
             <a href="/signup">SignUp</a>
+            <a href="/contact">Contact</a>
         </React.Fragment>
     )
 }
