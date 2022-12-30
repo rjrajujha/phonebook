861c6f64a793864860a9de591979e3dded5adfd6
rjrajujha <rjrajujha.com@outlook.com>
2022-12-30 11:54:44 +0530

    added all

---
+++ src/components/Filter.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+import filter from '../assests/images/filter.png';
+import line from '../assests/images/lineblack.png';
+import downarrow from '../assests/images/downarrow.png';
+function Filter() {
+  return (
+    <Filters className="flex a-center j-center gap">
+      <div><img src={filter} alt="filter"></img></div>
+      <div>Filter</div>
+      <div><img src={line} alt='line'></img></div>
+      <div><img src={downarrow} alt='arror'></img></div>
+     
+    </Filters>
+  );
+}
+
+export default Filter;
+const Filters=styled.div`
+position: absolute;
+width: 169px;
+height: 40px;
+left:18%;
+top:2%;
+
+background: #FFFFFF;
+box-shadow: 1px 1px 20px rgba(0, 0, 0, 0.1);
+border-radius: 10px;
+
+
+
+`
