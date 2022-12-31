742430a33e7b3eca385c0317e5257d804b0608d7
rjrajujha <rjrajujha.com@outlook.com>
2022-12-31 12:03:20 +0530

    sign up page optimised

--- src/pages/signUpPage/signup.jsx
+++ src/pages/signUpPage/signup.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
 import "./signup.css";
 import dots from "../Images/dots.svg";
 import topleft from "../Images/topleft.svg";
